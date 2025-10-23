307d3b22ad25a11f16cac4007ca08689d325a8b2
Julian Williams <julian@wazworld.net>
2025-10-22 22:29:49 -0500

    pointer/reference spacing style

--- TestApp/TestApp.cpp
+++ TestApp/TestApp.cpp
@@ -1,9 +1,26 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
 #include "../jawengine/JawEngine.h"
 
-static void loop(jaw::properties *props) {}
+jaw::bmpid bmp;
+jaw::argb *img;
+jaw::vec2i dim;
+
+static void init(jaw::properties *props) {
+	dim = asset::bmp("F:/assets/test-animation/idle-40x70x6.png", &img);
+	bmp = draw::createBmp(dim);
+	draw::writeBmp(bmp, img, dim);
+}
+
+static void loop(jaw::properties *props) {
+	draw::bmp(draw::bmpOptions{
+		.bmp = bmp,
+		.src = jaw::recti(0,dim),
+		.dest = jaw::recti(0,dim)
+	}, 0);
+}
 
 int main() {
 	jaw::properties props;
-	engine::start(&props, nullptr, nullptr, loop);
+	props.scale = 4;
+	engine::start(&props, nullptr, init, loop);
 }
